d455c07088d252d7f97ff528b74115c2d920d760
EQejvanaj <endrirdne@gmail.com>
2018-11-14 16:46:49 +0100

    Added En and Es Profiles

--- src/main/java/guru/springframework/didemo/services/SpanishPrimaryGreetingService.java
+++ src/main/java/guru/springframework/didemo/services/SpanishPrimaryGreetingService.java
@@ -8,12 +8,12 @@ import org.springframework.stereotype.Service;
  * Created by EQejvanaj on 14-Nov-18
  */
 @Service
+@Profile("es")
 @Primary
-@Profile("en")
-public class PrimaryGreetingService implements GreetingService {
+public class SpanishPrimaryGreetingService implements GreetingService {
 
     @Override
     public String sayGreeting() {
-        return "Hello - Primary Greeting service";
+        return "Servicio de Saludo Primario";
     }
 }
